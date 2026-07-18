4c9b9d11bf8ab689647d481622f277ddf7dda7bf
Muhammad Hamza Khan <khumza050@gmail.com>
2026-07-18 19:13:53 +0500

    Added streaming response

--- AI_tutor.py
+++ AI_tutor.py
@@ -2,6 +2,7 @@ from google import genai
 from dotenv import load_dotenv
 from google.genai import types
 import os
+import time
 load_dotenv()
 
 system_instruction = "You are an expert and patient AI tutor. If the user asks for factual information, definitions, lists, or recommendations, answer directly and clearly." \
@@ -14,9 +15,10 @@ if Token is None:
 
 
 def ask_AI_Tutor(prompt:str) -> str:
-    print("Asking your AI tutor on: ", prompt)
+    print("Generating response......")
+    full_stream_response = ""
     try:
-        response = client.models.generate_content(
+        streams = client.models.generate_content_stream(
             model = "gemini-2.5-flash" ,
             contents = prompt , 
             config=types.GenerateContentConfig(
@@ -25,20 +27,25 @@ def ask_AI_Tutor(prompt:str) -> str:
                 max_output_tokens= 3000,
             )
         )
-        return response.text
+        for chunks in streams:
+            if chunks.text:
+                print(chunks.text , end="" , flush=True)
+                full_stream_response +=chunks.text
+            
+        return full_stream_response
     except Exception as e:
         print("An error occured: ", e)
 
 
 def main():
     history = []
+    Max_history =20
     print("="*50)
     print("AI tutor")
     print("="*50)
     while True:
         try:
             prompt = input("Ask AI tutor: ")
-
             if prompt.lower().strip() in ["bye","goodbye","exit","quit","q","stop","end","close","leave","terminate","finish","done",
                           "see you","see ya","farewell","exit()","quit()"]:
                  print("Goodbye")
@@ -46,11 +53,14 @@ def main():
             else:
                 history.append(f"Users: {prompt}")
                 conversation_context = "\n".join(history)
+                start_time = time.time()
                 AI_response = ask_AI_Tutor(conversation_context)
-                print("\n")
-                print(f"AI tutor: {AI_response}")
                 history.append(f"AI tutor: {AI_response}")
-                
+                if len(history)>Max_history:
+                    history = history[-Max_history:]
+                end_time = time.time() 
+                total_time=round(end_time - start_time, 2)
+                print(f"\nResponse time taken: {total_time} seconds")
             
         except Exception as e:
             print("AI tutor is unavailable temporary")
